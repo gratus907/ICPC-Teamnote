361882566b4ce5c7984d3a69bc840ba265ff49cc
Wonseok Shin <gratus907@naver.com>
2021-07-05 23:58:09 +0900

    Update SegmentTree.h

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -7,32 +7,28 @@
  * Time: O(\log N)
  * Status: stress-tested
  */
-struct Segtree
+struct segtree
 {
-	int n;
-	int T[2*N];
-	void build(vector<int> &v)
-	{
-		n = v.size();
-		for (int i = 0; i<n; i++)
-			T[n+i] = v[i];
-		for (int i = n-1; i>0; i--)
-			T[i] = T[i<<1] + T[(i<<1)|1];
-	}
-	void modify(int pos, int val)
-	{
-		for (T[pos += n] = val; pos > 1; pos >>= 1)
-			T[pos >> 1] = T[pos] + T[pos^1];
-	}
-	// query is on [l, r)!!
-	int query(int l, int r)
-	{
-		int res = 0;
-		for (l += n, r += n; l < r; l >>=1, r>>=1)
-		{
-			if (l & 1) res += T[l++];
-			if (r & 1) res += T[--r];
-		}
-		return res;
-	}
-} S;
+    int n;
+    int T[2*N];
+    void build(vector<int> &v) {
+        n = v.size();
+        for (int i = 0; i<n; i++)
+            T[n+i] = v[i];
+        for (int i = n-1; i>0; i--)
+            T[i] = T[i<<1] + T[(i<<1)|1];
+    }
+    void modify(int pos, int val) {
+        for (T[pos += n] = val; pos > 1; pos >>= 1)
+            T[pos >> 1] = T[pos] + T[pos^1];
+    }
+    // query is on [l, r)!!
+    int query(int l, int r){
+        int res = 0;
+        for (l += n, r += n; l < r; l >>=1, r>>=1) {
+            if (l & 1) res += T[l++];
+            if (r & 1) res += T[--r];
+        }
+        return res;
+    }
+};
