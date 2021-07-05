2de38abb098347c78c5b9faf97f4c279c105c1f1
Wonseok Shin <gratus907@naver.com>
2021-07-06 01:50:18 +0900

    Update SegmentTree.h

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -9,26 +9,33 @@
  */
 struct segtree
 {
+    using elem = int;
     int n;
-    int T[2*N];
-    void build(vector<int> &v) {
+    elem T[2*N];
+
+    inline elem agg(elem a, elem b) {
+        return max(a, b);
+    }
+
+    void build(vector<elem> &v) {
         n = v.size();
         for (int i = 0; i<n; i++)
             T[n+i] = v[i];
         for (int i = n-1; i>0; i--)
-            T[i] = T[i<<1] + T[(i<<1)|1];
+            T[i] = agg(T[i<<1], T[(i<<1)|1]);
     }
     void modify(int pos, int val) {
         for (T[pos += n] = val; pos > 1; pos >>= 1)
-            T[pos >> 1] = T[pos] + T[pos^1];
+            T[pos >> 1] = agg(T[pos], T[pos^1]);
     }
     // query is on [l, r)!!
-    int query(int l, int r){
-        int res = 0;
+    elem query(elem l, elem r){
+        elem res = 0;
         for (l += n, r += n; l < r; l >>=1, r>>=1) {
-            if (l & 1) res += T[l++];
-            if (r & 1) res += T[--r];
+            if (l & 1) res = agg(T[l++], res);
+            if (r & 1) res = agg(T[--r], res);
         }
         return res;
     }
 };
+
