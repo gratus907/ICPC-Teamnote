28f21d298253cec67993732b0ad77bb1add51e49
Wonseok Shin <gratus907@snu.ac.kr>
2021-12-31 11:10:57 +0900

    Update UnionFind.h

--- content/data-structures/UnionFind.h
+++ content/data-structures/UnionFind.h
@@ -10,23 +10,20 @@ struct DSU
 {
     int par[V], sz[V];
     DSU(){init(V);}
-    void init(int n)
-    {
+    void init(int n) {
         for (int i = 0; i<n; i++)
         par[i] = i, sz[i] = 1;
     }
-    int find(int x)
-    {
+    int find(int x) {
         return x == par[x] ? x : (par[x] = find(par[x]));
     }
-    int getSize(int k){return sz[find(k)];}
-    void unite(int x, int y)
-    {
+    bool unite(int x, int y) {
         int u=find(x), v=find(y);
-        if(u==v) return;
+        if(u==v) return false;
         if(sz[u]>sz[v]) swap(u, v);
         sz[v]+=sz[u];
         sz[u] = 0;
         par[u] = par[v];
+        return true;
     }
 };
